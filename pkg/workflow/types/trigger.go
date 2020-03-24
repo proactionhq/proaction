6e1b0560602c4108283a68d8d0d04c9a612e4bb2
Marc Campbell <marc.e.campbell@gmail.com>
2020-03-24 13:46:56 -0700

    Update

--- pkg/workflow/types/trigger.go
+++ pkg/workflow/types/trigger.go
@@ -1,11 +1,14 @@
 package types
 
-import "github.com/pkg/errors"
+import (
+	"github.com/pkg/errors"
+	_ "gopkg.in/yaml.v2"
+)
 
 type Trigger struct {
-	Type    TriggerType
+	Type            TriggerType
 	StringOrListVal *StringOrList
-	MultiEventVal *MultiEvent
+	MultiEventVal   *MultiEvent
 }
 
 type TriggerType int
@@ -16,34 +19,33 @@ const (
 )
 
 type MultiEvent struct {
-	CheckRun *EventWithTypes `yaml:"check_run,omitempty"`
-	CheckSuite *EventWithTypes `yaml:"check_suite,omitempty"`
-	Create *EventWithoutTypes `yaml:"create,omitempty"`
-	Delete *EventWithoutTypes `yaml:"delete,omitempty"`
-	Deployment *EventWithoutTypes `yaml:"deployment,omitempty"`
-	DeploymentStatus *EventWithoutTypes `yaml:"deployment_status,omitempty"`
-	Fork *EventWithoutTypes `yaml:"fork,omitempty"`
-	Gollum *EventWithoutTypes `yaml:"gollum,omitempty"`
-	IssueComment *EventWithTypes `yaml:"issue_comment,omitempty"`
-	Issues *EventWithTypes `yaml:"issues,omitempty"`
-	Label *EventWithTypes `yaml:"label,omitempty"`
-	Milestone *EventWithTypes `yaml:"milestone,omitempty"`
-	PageBuild *EventWithoutTypes `yaml:"page_build,omitempty"`
-	Project *EventWithTypes `yaml:"project,omitempty"`
-	ProjectCard *EventWithTypes `yaml:"project_card,omitempty"`
-	ProjectColumn *EventWithTypes `yaml:"project_column,omitempty"`
-	Public *EventWithoutTypes `yaml:"public,omitempty"`
-	PullRequest *PushPullEvent `yaml:"pull_request,omitempty"`
-	PullRequestReview *EventWithTypes `yaml:"pull_request_review,omitempty"`
-	PullRequestReviewComment *EventWithTypes `yaml:"pull_request_review_comment,omitempty"`
-	Push *PushPullEvent `yaml:"push,omitempty"`	
-	RegistryPackage *EventWithTypes `yaml:"registry_package,omitempty"`
-	Release *EventWithTypes `yaml:"release,omitempty"`
-	Status *EventWithoutTypes `yaml:"status,omitempty"`
-	Watch *EventWithTypes `yaml:"watch,omitempty"`
-	Schedule *ScheduleEvent `yaml:"schedule,omitempty"`
-	RepositoryDispatch *EventWithoutTypes `yaml:"repository_dispatch,omitempty"`
-
+	CheckRun                 *EventWithTypes    `yaml:"check_run,omitempty"`
+	CheckSuite               *EventWithTypes    `yaml:"check_suite,omitempty"`
+	Create                   *EventWithoutTypes `yaml:"create,omitempty"`
+	Delete                   *EventWithoutTypes `yaml:"delete,omitempty"`
+	Deployment               *EventWithoutTypes `yaml:"deployment,omitempty"`
+	DeploymentStatus         *EventWithoutTypes `yaml:"deployment_status,omitempty"`
+	Fork                     *EventWithoutTypes `yaml:"fork,omitempty"`
+	Gollum                   *EventWithoutTypes `yaml:"gollum,omitempty"`
+	IssueComment             *EventWithTypes    `yaml:"issue_comment,omitempty"`
+	Issues                   *EventWithTypes    `yaml:"issues,omitempty"`
+	Label                    *EventWithTypes    `yaml:"label,omitempty"`
+	Milestone                *EventWithTypes    `yaml:"milestone,omitempty"`
+	PageBuild                *EventWithoutTypes `yaml:"page_build,omitempty"`
+	Project                  *EventWithTypes    `yaml:"project,omitempty"`
+	ProjectCard              *EventWithTypes    `yaml:"project_card,omitempty"`
+	ProjectColumn            *EventWithTypes    `yaml:"project_column,omitempty"`
+	Public                   *EventWithoutTypes `yaml:"public,omitempty"`
+	PullRequest              *PushPullEvent     `yaml:"pull_request,omitempty"`
+	PullRequestReview        *EventWithTypes    `yaml:"pull_request_review,omitempty"`
+	PullRequestReviewComment *EventWithTypes    `yaml:"pull_request_review_comment,omitempty"`
+	Push                     *PushPullEvent     `yaml:"push,omitempty"`
+	RegistryPackage          *EventWithTypes    `yaml:"registry_package,omitempty"`
+	Release                  *EventWithTypes    `yaml:"release,omitempty"`
+	Status                   *EventWithoutTypes `yaml:"status,omitempty"`
+	Watch                    *EventWithTypes    `yaml:"watch,omitempty"`
+	Schedule                 *ScheduleEvent     `yaml:"schedule,omitempty"`
+	RepositoryDispatch       *EventWithoutTypes `yaml:"repository_dispatch,omitempty"`
 }
 
 type EventWithTypes struct {
@@ -58,17 +60,17 @@ type ScheduleEvent struct {
 }
 
 type Cron struct {
-	CronField []string `yaml:"cron"`
+	CronField string `yaml:"cron"`
 }
 
 type PushPullEvent struct {
-	Types StringOrList `yaml:"types"`
-	Branches []string `yaml:"branches,omitempty"`
-	BranchesIgnore []string `yaml:"branches-ignore,omitempty"`
-	Tags []string `yaml:"tags,omitempty"`
-	TagsIgnore []string `yaml:"tags-ignore,omitempty"`
-	Paths []string `yaml:"paths,omitempty"`
-	PathsIgnore []string `yaml:"paths-ignore,omitempty"`
+	Types          StringOrList `yaml:"types"`
+	Branches       []string     `yaml:"branches,omitempty"`
+	BranchesIgnore []string     `yaml:"branches-ignore,omitempty"`
+	Tags           []string     `yaml:"tags,omitempty"`
+	TagsIgnore     []string     `yaml:"tags-ignore,omitempty"`
+	Paths          []string     `yaml:"paths,omitempty"`
+	PathsIgnore    []string     `yaml:"paths-ignore,omitempty"`
 }
 
 func (t *Trigger) UnmarshalYAML(unmarshal func(interface{}) error) error {
--- pkg/workflow/types/trigger_test.go
+++ pkg/workflow/types/trigger_test.go
@@ -27,6 +27,45 @@ func Test_Trigger_UnmarshalYAML(t *testing.T) {
 			},
 			wantMultiEventVal: nil,
 		},
+		{
+			name:     "list",
+			input:    "[foo, baz]",
+			wantType: StrOrListType,
+			wantStringOrListVal: &StringOrList{
+				Type:   List,
+				StrVal: nil,
+				ListVal: []string{
+					"foo",
+					"baz",
+				},
+			},
+			wantMultiEventVal: nil,
+		},
+		{
+			name:                "create",
+			input:               "create:",
+			wantType:            MultiEventType,
+			wantStringOrListVal: nil,
+			wantMultiEventVal: &MultiEvent{
+				Create: &EventWithoutTypes{},
+			},
+		},
+		// 		{
+		// 			name: "schedule",
+		// 			input: `schedule:
+		//   - cron: "0 */4 * * *"`,
+		// 			wantType:            MultiEventType,
+		// 			wantStringOrListVal: nil,
+		// 			wantMultiEventVal: &MultiEvent{
+		// 				Schedule: &ScheduleEvent{
+		// 					Crons: []Cron{
+		// 						Cron{
+		// 							CronField: "0 */4 * * *",
+		// 						},
+		// 					},
+		// 				},
+		// 			},
+		// 		},
 	}
 
 	for _, test := range tests {
